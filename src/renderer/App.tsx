de5432066650c0a38334d0241f51fe0a3fe416eb
SpoonOfDoom <christian@syska.me>
2023-11-16 22:18:28 +0100

    chore: commit electron-react-boilerplate template

---
+++ src/renderer/App.tsx
@@ -0,0 +1,27 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+function Hello() {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200" alt="icon" src={icon} />
+      </div>
+      <h1>Spoony Initiative</h1>
+      <div className="Hello">
+        <p>Hello!</p>
+      </div>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
