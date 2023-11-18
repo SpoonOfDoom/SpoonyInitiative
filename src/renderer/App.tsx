046378a6168728a747db87e2456e4e88c369add1
SpoonOfDoom <christian@syska.me>
2023-11-19 00:43:06 +0100

    feat: put InitiativeView in start screen with some participants This is temporary for development and will be moved to a more sensible place later.

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,24 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import { MemoryRouter as Router, Route, Routes } from 'react-router-dom';
 import icon from '../../assets/icon.svg';
 import './App.css';
+import { ParticipantProps } from '../components/Participant';
+import InitiativeView from '../components/InitiativeView';
 
-function Hello() {
+function StartScreen() {
+  const participants: ParticipantProps[] = [
+    { name: 'Bob', initiative: 10 },
+    { name: 'Alice', initiative: 20 },
+    { name: 'Charlie', initiative: 25 },
+  ];
+
   return (
     <div>
+      <h1 style={{ textAlign: 'center' }}>Spoony Initiative</h1>
       <div className="Hello">
         <img width="200" alt="icon" src={icon} />
       </div>
-      <h1>Spoony Initiative</h1>
-      <div className="Hello">
-        <p>Hello!</p>
-      </div>
+
+      <InitiativeView participants={participants} />
     </div>
   );
 }
@@ -20,7 +27,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<StartScreen />} />
       </Routes>
     </Router>
   );
